f11cf4753953279d9acb2df38485e0ff5c521818
Antognia <168127637+Antognia@users.noreply.github.com>
2024-06-19 13:01:08 +0200

    Es. aggiornato e corretto.js

--- Funzione_2/main.js
+++ Funzione_2/main.js
@@ -4,9 +4,9 @@ for (let i = 1; i <= N; i++) {
     if (i % 15 == 0) {
         scala.push('fizzBuzz');
     } else if(i % 3 == 0) {
-        scala.push('Buzz');
-    }else if(i % 5 == 0){
         scala.push('Fizz');
+    }else if(i % 5 == 0){
+        scala.push('Buzz');
     }else {
         scala.push(i);
     }
